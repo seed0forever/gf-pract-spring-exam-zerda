16a76f6f4adf24fff918fd6e3a954892259c298a
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-31 10:34:09 +0200

    initial project commit

---
+++ feedbacksite/src/main/java/com/grenfox/exampractice/spring/feedbacksite/FeedbacksiteApplication.java
@@ -0,0 +1,12 @@
+package com.grenfox.exampractice.spring.feedbacksite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbacksiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbacksiteApplication.class, args);
+	}
+}
