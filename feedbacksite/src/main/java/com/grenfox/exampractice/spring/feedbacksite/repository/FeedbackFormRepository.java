5f492c08e158e2890e55c4a8d6370a8aef35375b
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-01 15:38:31 +0200

    add @Repository annotation

--- feedbacksite/src/main/java/com/grenfox/exampractice/spring/feedbacksite/repository/FeedbackFormRepository.java
+++ feedbacksite/src/main/java/com/grenfox/exampractice/spring/feedbacksite/repository/FeedbackFormRepository.java
@@ -2,7 +2,9 @@ package com.grenfox.exampractice.spring.feedbacksite.repository;
 
 import com.grenfox.exampractice.spring.feedbacksite.model.FeedbackForm;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeedbackFormRepository extends CrudRepository<FeedbackForm, Long> {
 
 }
